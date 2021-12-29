f3eab6daca97c938fdabb2fe501e616a0d403d42
nika1998 <95859106+nika1998@users.noreply.github.com>
2021-12-29 21:06:11 +0400

    Delete custom.js

--- custom.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('.chat_icon').click(function() {
-        $('.chat_box').toggleClass('active');
-    });
-
-    $('.conv-form-wrapper').convform({selectInputStyle: 'disable'})
-});
